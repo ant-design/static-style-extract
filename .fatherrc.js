752bf0aaac818a7c726b92002ad58d95ad8fabe6
wuxh <wxh1220@gmail.com>
2023-12-05 18:51:56 +0800

    chore: add file

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,4 +2,5 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   plugins: ['@rc-component/father-plugin'],
+  esm: { ignores: ['src/cli/**'] }
 });
---
+++ src/cli/index.ts
@@ -0,0 +1,8 @@
+
+console.log('Welcome to the CLI!')
+
+function main() {
+  console.log('Hello world!')
+}
+
+module.exports = main
