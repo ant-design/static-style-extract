0612c6c0a3ee457e37828a137a6f54dde61dea9e
二货机器人 <smith3816@gmail.com>
2024-08-28 20:09:55 +0800

    fix: warning of extract

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import {
   createCache,
   extractStyle as extStyle,
   StyleProvider,
 } from '@ant-design/cssinjs';
 import * as antd from 'antd';
+import React from 'react';
 import { renderToString } from 'react-dom/server';
 import type { CustomRender } from './interface';
+
 const blackList: string[] = [
   'ConfigProvider',
   'Drawer',
@@ -18,6 +19,26 @@ const blackList: string[] = [
   'Tour',
 ];
 
+const ComponentCustomizeRender: Record<
+  string,
+  (component: React.ComponentType<any>) => React.ReactNode
+> = {
+  Affix: (Affix) => (
+    <Affix>
+      <div />
+    </Affix>
+  ),
+  BackTop: () => <antd.FloatButton.BackTop />,
+  Dropdown: (Dropdown) => (
+    <Dropdown menu={{ items: [] }}>
+      <div />
+    </Dropdown>
+  ),
+  Menu: (Menu) => <Menu items={[]} />,
+  QRCode: (QRCode) => <QRCode value="https://ant.design" />,
+  Tree: (Tree) => <Tree treeData={[]} />,
+};
+
 const defaultNode = () => (
   <>
     {Object.keys(antd)
@@ -27,16 +48,15 @@ const defaultNode = () => (
       )
       .map((compName) => {
         const Comp = antd[compName];
-        if (compName === 'Dropdown') {
+
+        const renderFunc = ComponentCustomizeRender[compName];
+
+        if (renderFunc) {
           return (
-            <Comp
-              key={compName}
-              menu={{ items: [] }}
-            >
-                <div />
-            </Comp>
+            <React.Fragment key={compName}>{renderFunc(Comp)}</React.Fragment>
           );
         }
+
         return <Comp key={compName} />;
       })}
   </>
